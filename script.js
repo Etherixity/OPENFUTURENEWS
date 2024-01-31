668dd08609e6fbad3c10404978da9dc3b70271cd
Etherixity <157638381+Etherixity@users.noreply.github.com>
2024-01-31 18:23:47 +0000

    posting added

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
-// Assuming you have a form with id "blog-form", input with id "password-input" and textarea with id "blog-text"
-document.getElementById('blog-form').addEventListener('submit', function(event) {
-  event.preventDefault();
-  var password = document.getElementById('password-input').value;
-  if (password === 'qwerty') {
-    var post = document.getElementById('blog-text').value;
-    document.getElementById('news-content').innerText = post;
-  } else {
-    alert('Incorrect password');
-  }
-});
+fetch('post.txt')
+  .then(response => response.text())
+  .then(data => {
+    document.getElementById('post-content').innerText = data;
+  });
+
+
+const updatePostContent = () => {
+  fetch('post.txt')
+    .then(response => response.text())
+    .then(data => {
+      document.getElementById('post-content').innerText = data;
+    });
+};
+
+setInterval(updatePostContent, 5000);
